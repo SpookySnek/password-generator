8e374b6c68bd8c0e3565120964293fe36d379a3e
SpookySnek <carleric.olsson@gmail.com>
2023-02-20 12:29:34 +0100

    Implemented basic functionality

--- password-generator/Form1.Designer.cs
+++ password-generator/Form1.Designer.cs
@@ -28,19 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(494, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 93);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Password";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(76, 56);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(644, 208);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Tag = "txtPassword";
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 583);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "v";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox txtPassword;
     }
 }
--- password-generator/Form1.cs
+++ password-generator/Form1.cs
@@ -6,5 +6,25 @@ namespace password_generator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;':\",./<>?";
+            Random random = new Random();
+            int passwordLength = 12;
+            char[] password = new char[passwordLength];
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                password[i] = validChars[random.Next(validChars.Length)];
+            }
+
+            txtPassword.Text = new string(password);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
